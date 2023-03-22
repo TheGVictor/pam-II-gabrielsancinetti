6360f687e61ff212427efefb66a4973b6822db60
TheGVictor <gabrielsancinetti@gmail.com>
2023-03-22 08:29:26 -0300

    Foram criados dois arquivos em JS

---
+++ aula02/funcoesevariaveis.js
@@ -0,0 +1,18 @@
+let num1 = 2; 
+const num2 = 3
+var num3 = 5
+var msg = "Olá lindos. Como estão? Esse é a idade mental do Kayky:"
+let usuario = {
+    nome: "Kayky", 
+    idade: 35,
+    signo: "Virgem",
+    calvo: false
+}
+
+// console.log(`${msg} ${num3} aninhos. a primeira variável é do tipo ${typeof msg}. Já a segunda, é do tipo ${typeof num3}`)
+
+console.log(`${usuario.nome} é feio e tem ${usuario.idade} anos, e ainda é ${usuario.signo} pois nasceu dia 23 de agosto!!!!`)
+
+console.log(typeof usuario.calvo)
+
+
