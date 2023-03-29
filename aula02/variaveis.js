68602b4ec38a1ee6506ab7b85a8fb882c243a465
TheGVictor <gabrielsancinetti@gmail.com>
2023-03-29 08:31:19 -0300

    Conteúdo da aula 3 inserido

---
+++ aula02/variaveis.js
@@ -0,0 +1,10 @@
+let num1 = 2; 
+const num2 = 3
+var num3 = 5
+var msg = "Olá lindos. Como estão? Esse é a idade mental do Kayky:"
+let usuario = {
+    nome: "Kayky", 
+    idade: 35,
+    signo: "Virgem",
+    calvo: false
+}
---
+++ aula03/funcoes.js
@@ -0,0 +1,24 @@
+function msgTeste() {
+    console.log('Funcionou que é uma beleza!')
+}
+
+msgTeste()
+
+function msgTesteArgumento(param) {
+    console.log(`O parâmetro passado foi: ${param}. O seu tipo é ${typeof param}`)
+}
+
+msgTesteArgumento('olá')
+msgTesteArgumento(80)
+
+function soma (n1,n2) {
+    let result;
+    
+    if(typeof n1 == "number" && typeof n2 == "number") {
+        return result = n1 + n2
+    }
+
+    return result = "Deu bom não"
+}
+console.log(soma('13', 17))
+console.log(soma(13, 17))
